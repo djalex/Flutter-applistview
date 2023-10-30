08dcf1dace5f15f6c4209b1539f58640188a833b
Alexandro Sousa <djalexthe@gmail.com>
2023-10-29 21:23:13 -0300

    teste

---
+++ android/app/src/main/kotlin/com/example/applistview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.applistview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Using list view",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: <Widget> [
+          Container(
+            height: 50,
+            color: Colors.amber[600],
+            child: const Center(child: Text("Entry A"),) 
+          ),
+          Container(
+            height: 50,
+            color: Colors.amber[500],
+            child: const Center(child: Text("Entry B"),)
+          ),
+          Container(
+            height: 50,
+            color: Colors.amber[100],
+            child: const Center(child: Text("Entry C"),)
+          ),
+
+        ],
+ 
+      )
+
+    );
+  }
+}
+
